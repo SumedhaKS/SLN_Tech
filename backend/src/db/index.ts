048c8d49389cf2ec0d711224cd20941ae42c647a
SumedhaKS <kssumedha24@gmail.com>
2025-03-13 22:35:54 +0530

    2 endpoints done

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,17 +1,46 @@
 import { Client } from "pg";
 require('dotenv').config()
 
-const client = new Client({connectionString:process.env.DATABASE_URL})
+export const client = new Client({connectionString:process.env.DATABASE_URL})
 client.connect()
 
-const initialSetUp = ()=>{
+const initialSetUp = async ()=>{
     //create table properly
     // alter sequence to start from 1000; 
+    const q1 = await client.query(`CREATE TABLE customer(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE,
+    phNo VARCHAR(15) UNIQUE NOT NULL,
+    address TEXT NOT NULL,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    CREATE TABLE service(
+    id SERIAL PRIMARY KEY,
+    customer_ID INT REFERENCES customer(id) ON DELETE CASCADE,
+    model VARCHAR(50) NOT NULL,
+    make VARCHAR(50) NOT NULL,
+    serial_no VARCHAR(20) UNIQUE,
+    issue_description TEXT NOT NULL,
+    status VARCHAR(15) DEFAULT 'pending',
+    estimated_cost decimal(10,2),
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    `)
+    
+    const q2 = await client.query(`ALTER SEQUENCE customer_id_seq RESTART 1000;`)
 
+    console.log("initial setup successfull.")
 }
-
 // initialSetUp()
 
-module.exports= {
-    client
+const populateDB = async ()=>{
+    await client.query(`INSERT INTO customer(name, phno, address) VALUES('user1', '1234567890', 'address1'), ('user2', '0123456789', 'address2')`)
+    console.log('done')
 }
+// populateDB()
+
+
+module.exports = { client };
+    
+
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,89 @@
 // main index file
+import express from "express";
+import { client } from "./db"
+import zod from "zod";
+
+const app = express()
+const port = 3001;
+
+app.use(express.json())
+
+app.get('/', (req:any, res:any)=>{
+    res.send("Hello")
+})
+
+app.get('/jobs', async (req:any, res:any)=>{
+    try{
+        const response = await client.query(`SELECT id FROM customer;`) // returns an array of object of IDs 
+        console.log(typeof(response.rows))
+        res.json({
+            message : response.rows 
+        })
+    }
+   catch(err){
+    console.error("error : " + err)
+   }
+})
+
+const userSchema = zod.object({
+    name: zod.string(),
+    email: zod.string().email().optional(),
+    phno: zod.string().min(10),
+    address: zod.string()
+})
+
+app.post('/customer-details', async (req:any ,res:any)=>{  
+    try{
+        console.log(req.body)
+        const userBody = userSchema.safeParse(req.body);
+        console.log(userBody)
+        if(!userBody.success){
+            return res.status(411).json({
+                message: "Invalid inputs"
+            })
+        }
+        else{
+            if(!req.body.email){
+                const query = `INSERT INTO customer(name, phno, address)values($1, $2, $3);`
+                const values = [req.body.name, req.body.phno, req.body.address]
+                const queryResponse = await client.query(query, values)
+                if(!queryResponse){
+                    return res.json({
+                        message : "Try again later"
+                    })
+                }  
+            }
+            else{
+                const query = `INSERT INTO customer(name, email, phno, address)values($1, $2, $3, $4)`
+                const values = [req.body.name, req.body.email, req.body.phno, req.body.address]
+                const queryResponse = await client.query(query, values)
+                if(!queryResponse){
+                    return res.json({
+                    'message' : "Try again later"
+                    })
+                }
+                else{
+                    return res.json({
+                        message : "User created successfully"
+                    })
+                }
+            }
+        }
+    } catch(err){
+        console.error("Error occurred:", err)
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    } 
+    // finally{
+    //     await client.end()
+    // }       
+})
+
+app.post('/job-details', (req:any,res:any)=>{
+    
+})
+
+
+
+app.listen(port, ()=> console.log(`listening on port ${port}`))
